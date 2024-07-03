8e1c072d330f8a66bf75efe87f6ca64a8080d9c4
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-07-03 18:44:22 +0600

    OOP

---
+++ Class And Objects/class_object.cpp
@@ -0,0 +1,55 @@
+/***************************************
+Author: Md Rakibul Hasan
+date: 2024-07-03 18:20:54
+****************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class teacher
+{
+private:
+    double selary;
+
+public:
+    // propertics || atributes
+    string name;
+    string dept;
+    string subject;
+
+    // methods || member function
+    void changeDept(int newDpt)
+    {
+        dept = newDpt;
+    }
+
+    // setter
+    void setSelary(int s)
+    {
+        selary = s;
+    }
+    // getter
+    double getSelary()
+    {
+        return selary;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    teacher t1;
+
+    t1.name = "Shuvo";
+    t1.dept = "CSE";
+    t1.subject = "Programming";
+    // t1.selary = 25000;
+    t1.setSelary(25000);
+
+    cout << t1.name << endl;
+    cout << t1.getSelary() << endl;
+
+    return 0;
+}
